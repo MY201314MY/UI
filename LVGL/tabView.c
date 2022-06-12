7a195fdd9a156202fecd6b6455383823f073d871
mongoose <36258286+MY201314MY@users.noreply.github.com>
2022-06-12 22:39:11 +0800

    slide

---
+++ LVGL/tabView.c
@@ -0,0 +1,120 @@
+﻿/**
+ * @file lv_demo_widgets.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lv_demo_widgets.h"
+
+#if LV_USE_DEMO_WIDGETS
+
+#if LV_MEM_CUSTOM == 0 && LV_MEM_SIZE < (38ul * 1024ul)
+    #error Insufficient memory for lv_demo_widgets. Please set LV_MEM_SIZE to at least 38KB (38ul * 1024ul).  48KB is recommended.
+#endif
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void profile_create(lv_obj_t * parent);
+static void analytics_create(lv_obj_t * parent);
+static void shop_create(lv_obj_t * parent);
+
+void lv_demo_widgets(void)
+{
+    lv_obj_t *tv = lv_tabview_create(lv_scr_act(), LV_DIR_TOP, -48);
+    
+    lv_obj_align(tv, LV_ALIGN_TOP_MID, 0, 48);
+
+    lv_obj_t * t1 = lv_tabview_add_tab(tv, "ABC");
+    lv_obj_t * t2 = lv_tabview_add_tab(tv, "XYZ");
+    lv_obj_t * t3 = lv_tabview_add_tab(tv, "LMN");
+    profile_create(t1);
+    analytics_create(t2);
+    shop_create(t3);
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+static void profile_create(lv_obj_t * parent)
+{
+    lv_obj_t * label1 = lv_label_create(parent);
+    lv_label_set_long_mode(label1, LV_LABEL_LONG_WRAP);     /*Break the long lines*/
+    lv_label_set_recolor(label1, true);                      /*Enable re-coloring by commands in the text*/
+    lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label, align the lines to the center "
+                      "and wrap long text automatically.");
+    lv_obj_set_width(label1, 150);  /*Set smaller width to make the lines wrap*/
+    lv_obj_set_style_text_align(label1, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_align(label1, LV_ALIGN_CENTER, 0, -40);
+
+    lv_obj_t * label2 = lv_label_create(parent);
+    lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR);     /*Circular scroll*/
+    lv_obj_set_width(label2, 150);
+    lv_label_set_text(label2, "It is a circularly scrolling text. ");
+    lv_obj_align(label2, LV_ALIGN_CENTER, 0, 40);
+}
+
+
+static void analytics_create(lv_obj_t * parent)
+{
+    /*Create a LED and switch it OFF*/
+    lv_obj_t * led1  = lv_led_create(parent);
+    lv_obj_align(led1, LV_ALIGN_CENTER, -80, 0);
+    lv_led_off(led1);
+
+    /*Copy the previous LED and set a brightness*/
+    lv_obj_t * led2  = lv_led_create(parent);
+    lv_obj_align(led2, LV_ALIGN_CENTER, 0, 0);
+    lv_led_set_brightness(led2, 150);
+    lv_led_set_color(led2, lv_palette_main(LV_PALETTE_RED));
+
+    /*Copy the previous LED and switch it ON*/
+    lv_obj_t * led3  = lv_led_create(parent);
+    lv_obj_align(led3, LV_ALIGN_CENTER, 80, 0);
+    lv_led_on(led3);
+}
+
+static void event_handler(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        LV_LOG_USER("Clicked");
+    }
+    else if(code == LV_EVENT_VALUE_CHANGED) {
+        LV_LOG_USER("Toggled");
+    }
+}
+
+void shop_create(lv_obj_t * parent)
+{
+    lv_obj_t * label;
+
+    lv_obj_t * btn1 = lv_btn_create(parent);
+    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+
+    label = lv_label_create(btn1);
+    lv_label_set_text(label, "Button");
+    lv_obj_center(label);
+
+    lv_obj_t * btn2 = lv_btn_create(parent);
+    lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
+    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+    lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+
+    label = lv_label_create(btn2);
+    lv_label_set_text(label, "Toggle");
+    lv_obj_center(label);
+}
+
+#endif
+ 
